6d45e129dd5a94402cf17d979b09dea8011ed008
essviv <514912821@qq.com>
2016-04-12 19:20:32 +0800

    init commit

---
+++ src/main/java/com/cmcc/syw/App.java
@@ -0,0 +1,13 @@
+package com.cmcc.syw;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
